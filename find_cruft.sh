1c42cf5d7f1681357602e3765d4e82a6ef281dfa
Bing Sun <subi.the.dream.walker@gmail.com>
2013-01-18 19:35:37 +0800

    find_cruft.sh: more omitted paths.

--- find_cruft.sh
+++ find_cruft.sh
@@ -4,7 +4,7 @@ cd /root
 
 cave show installed-slots::installed | grep '/' |xargs -I '{}' cave contents '{}' | awk '{print $1}' |sort -u >managed_file.list
 
-find / -mount -regextype posix-awk -regex '/run|/home|/root|/usr/src|/var/db/paludis|/var' -prune -o -print |sort -u >all_file.list
+find / -mount -regextype posix-awk -regex '/etc/ssl|/etc/paludis|/archlinux-i686|/run|/home|/root|/usr/src|/var/db/paludis|/var' -prune -o -print |sort -u >all_file.list
 
 diff all_file.list managed_file.list | grep '<' > unmanaged.list
 diff all_file.list managed_file.list | grep '>' > missing.list
